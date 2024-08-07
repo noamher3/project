f49afbc8223dcbe269cb3e3489574f99dd9acbc1
Noam <noamher3@gmail.com>
2024-08-07 16:52:46 +0300

    phase 7 features

--- controllers/memeController.js
+++ controllers/memeController.js
@@ -25,15 +25,26 @@ function renderMeme() {
   elImg.src = gImgs.find((img) => img.id === meme.selectedImgId).url;
   elImg.onload = () => {
     ctx.drawImage(elImg, 0, 0, elCanvas.width, elCanvas.height);
-    ctx.textAlign = "center";
     for (let i = 0; i < meme.lines.length; i++) {
       const txtObject = meme.lines[i];
-      ctx.font = `${txtObject.size}px serif`;
+      ctx.textAlign = txtObject.alignment ?? "center";
+      if (txtObject.x === undefined) {
+        saveLinePosition(i, 0, txtObject.size * i);
+      }
+      if (i === meme.selectedLineIdx) {
+        ctx.strokeStyle = "white";
+
+        ctx.strokeRect(0, txtObject.y + 8, elCanvas.width, txtObject.size);
+      }
+      ctx.font = `${txtObject.size}px ${txtObject.font ?? "serif"}`;
       ctx.fillStyle = txtObject.color;
+
+      console.log(txtObject.y);
+
       ctx.fillText(
         txtObject.txt,
         elCanvas.width * 0.5,
-        txtObject.size * (i + 1)
+        txtObject.y + txtObject.size
       );
     }
   };
@@ -45,4 +56,72 @@ function downloadImg(elLink) {
   elLink.href = imgContent;
   elLink.download = "amazingFile.jpeg";
   link.click();
+}
+
+function onSetColorText(value) {
+  setColorText(value);
+  renderMeme();
+}
+
+function onSetSizeText(value) {
+  setSizeText(value);
+  renderMeme();
+}
+
+function onAddLine() {
+  addLine();
+  renderMeme();
+}
+
+function onSwitchLine() {
+  switchLine();
+  renderMeme();
+}
+
+function onCanvasClick(event) {
+  const elCanvas = document.querySelector(".meme-canvas");
+  const meme = getMeme();
+
+  const lineClicked = meme.lines.find(
+    (line) =>
+      event.offsetX >= line.x &&
+      event.offsetX <= line.x + elCanvas.width &&
+      event.offsetY >= line.y &&
+      event.offsetY <= line.y + line.size
+  );
+  const textInput = document.querySelector(".text-input");
+  const colorInput = document.querySelector(".color-input");
+  const fontSelect = document.querySelector(".font-family-button");
+  const inputFontSize = document.querySelector(".input-font-size");
+  if (lineClicked) {
+    setSelectedLine(lineClicked);
+    textInput.value = lineClicked.txt;
+    colorInput.value = lineClicked.color;
+    fontSelect.value = lineClicked.font ?? "serif";
+    inputFontSize.value = lineClicked.size;
+  } else {
+    setSelectedLine(null);
+    document.querySelector(".control-panel").reset();
+  }
+  renderMeme();
+}
+
+function onFontSelected(value) {
+  setFontSelected(value);
+  renderMeme();
+}
+
+function onAlignFont(value) {
+  setAlignFont(value);
+  renderMeme();
+}
+
+function onInputTextSize(value) {
+  inputTextSize(value);
+  renderMeme();
+}
+
+function onArrowClick(value) {
+  arrowClick(value);
+  renderMeme();
 }
--- services/memeService.js
+++ services/memeService.js
@@ -2,10 +2,10 @@
 
 var gMeme = {
   selectedImgId: 1,
-  selectedLineIdx: 0,
+  selectedLineIdx: -1,
   lines: [
-    { txt: "Text line", size: 48, color: "red" },
-    { txt: "I sometimes eat Falafel", size: 48, color: "red" },
+    { txt: "Text line", size: 48, color: "#ff0000" },
+    { txt: "I sometimes eat Falafel", size: 48, color: "#ff0000" },
   ],
 };
 
@@ -14,9 +14,55 @@ function getMeme() {
 }
 
 function setLineTxt(txt) {
-  gMeme.lines[0].txt = txt;
+  gMeme.lines[gMeme.selectedLineIdx].txt = txt;
 }
 
 function setImg(id) {
   gMeme.selectedImgId = id;
+}
+
+function setColorText(value) {
+  gMeme.lines[gMeme.selectedLineIdx].color = value;
+}
+
+function setSizeText(value) {
+  value === "+"
+    ? (gMeme.lines[gMeme.selectedLineIdx].size += 2)
+    : (gMeme.lines[gMeme.selectedLineIdx].size -= 2);
+}
+
+function addLine() {
+  gMeme.lines.push({ txt: "Text line", size: 48, color: "green" });
+}
+
+function switchLine() {
+  gMeme.selectedLineIdx = (gMeme.selectedLineIdx + 1) % gMeme.lines.length;
+}
+
+function saveLinePosition(i, x, y) {
+  gMeme.lines[i].x = x;
+  gMeme.lines[i].y = y;
+}
+
+function setSelectedLine(line) {
+  gMeme.selectedLineIdx = gMeme.lines.findIndex((l) => l === line);
+}
+
+function setFontSelected(value) {
+  if (gMeme.selectedLineIdx === -1) return;
+  gMeme.lines[gMeme.selectedLineIdx].font = value;
+}
+
+function setAlignFont(value) {
+  gMeme.lines[gMeme.selectedLineIdx].alignment = value;
+}
+
+function inputTextSize(value) {
+  gMeme.lines[gMeme.selectedLineIdx].size = value;
+}
+
+function arrowClick(value) {
+  value === "+"
+    ? (gMeme.lines[gMeme.selectedLineIdx].y += 5)
+    : (gMeme.lines[gMeme.selectedLineIdx].y -= 5);
 }
